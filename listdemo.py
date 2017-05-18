8c0539bdc009641655536dc92c1ae6aae5c60aba
Simson Garfinkel <simson.l.garfinkel@census.gov>
2017-05-18 13:07:37 -0400

    added listdemo

---
+++ listdemo.py
@@ -0,0 +1,66 @@
+import sys
+from PyQt5.QtCore import QDate, QSize, Qt
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+# For ideas, see:
+# http://nullege.com/codes/show/src@p@y@pyqt5-HEAD@examples@dialogs@configdialog@configdialog.py/220/PyQt5.QtWidgets.QListWidgetItem.setTextAlignment/python
+
+class VerifyDialog(QDialog):
+    def __init__(self, parent=None):
+        super(VerifyDialog, self).__init__(parent)
+
+        self.listWidget = QListWidget()
+
+        for i in range(100):
+            item = QListWidgetItem("Item %i" % i)
+            # could be Qt.Unchecked; setting it makes the check appear
+            item.setCheckState(Qt.Checked) 
+            self.listWidget.addItem(item)
+
+        #self.listWidget.show()
+
+        #self.pagesWidget = QStackedWidget()
+        #self.pagesWidget.addWidget(ConfigurationPage())
+        #self.pagesWidget.addWidget(UpdatePage())
+        #self.pagesWidget.addWidget(QueryPage())
+  
+  
+        #self.createIcons()
+        #self.contentsWidget.setCurrentRow(0)
+  
+        runButton = QPushButton("Run")
+        runButton.clicked.connect(self.exec)
+
+        cancelButton = QPushButton("Cancel")
+        cancelButton.clicked.connect(self.cancel)
+  
+        horizontalLayout = QHBoxLayout()
+        #horizontalLayout.addWidget(self.contentsWidget)
+        #horizontalLayout.addWidget(self.pagesWidget, 1)
+        horizontalLayout.addWidget(self.listWidget, 1)
+  
+        buttonsLayout = QHBoxLayout()
+        buttonsLayout.addStretch(1)
+        buttonsLayout.addWidget(runButton)
+        buttonsLayout.addWidget(cancelButton)
+        
+  
+        mainLayout = QVBoxLayout()
+        mainLayout.addLayout(horizontalLayout)
+        mainLayout.addStretch(1)
+        mainLayout.addSpacing(12)
+        mainLayout.addLayout(buttonsLayout)
+  
+        self.setLayout(mainLayout)
+        self.setWindowTitle("Config Dialog")
+        self.show()
+
+    def exec(self):
+        print("Exec!")
+
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    dialog = VerifyDialog()
+    sys.exit(app.exec_())
--- scanner_test.py
+++ scanner_test.py
@@ -4,7 +4,13 @@ from scanner import *
 import zipfile
 
 def test_hash_file():
-    assert hash_file(open("test/hello.txt","rb"))=="e59ff97941044f85df5297e1c302d260"
+    # The first hash is on Unix, the second is on Windows
+    unix_hash = "e59ff97941044f85df5297e1c302d260"
+    windows_hash = "3579c8da7f1e0ad94656e76c886e5125"
+    assert hash_file(open("test/hello.txt","rb")) in [unix_hash,windows_hash]
+
+    # When hashing the binary file, it's always the same
+    assert hash_file(open("test/hello.zip","rb"))=="46d0707ff89bed468e78b4fcddf0ad60"  
 
 def test_open_zipfile():
     assert open_zipfile("scanner_test.py")==None
