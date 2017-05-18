566eb2118e88ba7fc9775be53d525366c6d570a6
Simson Garfinkel <simsong@acm.org>
2017-05-17 20:46:05 -0400

    fixed timestamps working at command line

---
+++ fix_timestamps.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python36
+#
+# fix_timestamps.py:
+# Scan the filesystem for timestamps in MMDDYY format and change them to YYYY-MM-DD
+#
+
+# Todo: 
+# - handle names
+
+import sys
+import os
+import re
+
+# Patterns to skip
+skip_pats = [re.compile("[0-9a-f]{16,}",re.I), # 16 hex digits
+             re.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",re.I), # GUID
+             re.compile("[12][012389][0-9][0-9]-[01][0-9]-[0-3][0-9]") # ISO-8601
+             ]
+
+mdy_pats  = [re.compile("[^0-9]([0-1][0-9])[.]?([0-3][0-9])[.]?(([12][90])?[89012][0-9])[^0-9]"), # MMDDYYYY
+        re.compile("[^0-9]([0-1]?[0-9])[.]([0-3]?[0-9])[.]([89012][0-9])[^0-9]") # M.D.YY     (people who are careless)
+        ]
+# Dirs to skip
+skip_dirs = set(["/assets/","/mail downloads/", "conda-meta", "site-packages"])
+
+# Extensions to skip
+skip_exts = set([".css",".js", ".jar", ".webhistory", ".ics"])
+
+
+def is_skipdir(fname):
+    fname_lower = fname.lower()
+    for d in skip_dirs:
+        if d in fname_lower:
+            return True
+    return False
+        
+def is_skip_pat(fname):
+    basename = os.path.basename(fname)
+    for s in skip_pats:
+        if s.search(basename):
+            return True
+    return False
+    
+
+def make_year_digit4(year):
+    assert len(year) in [2,4]
+    if len(year)==2:
+        if year[0] > '5':
+            return "19" + year
+        else:
+            return "20" + year
+    return year
+
+def make_digit2(val):
+    if len(val)==1:
+        return "0" + val
+    return val
+
+def valid_year(year):   return 1980 <= int(year) <= 2030
+def valid_month(month): return 1 <= int(month) <= 12
+def valid_day(day):     return 1 <= int(day) <= 31
+
+def newname(fname):
+    """Look at a name and see if the name should be renamed. Return the new name, or None if no rename is necessary."""
+
+    if is_skipdir(fname):
+        return None
+
+    dirname  = os.path.dirname(fname)
+    basename = os.path.basename(fname)
+    ext      = os.path.splitext(basename)[1]
+
+    if ext in skip_exts:
+        return None
+
+    if is_skip_pat(basename):
+        return None
+
+    for p in mdy_pats:
+        m = p.search(basename)
+        if m:
+            (month,day,year) = m.group(1,2,3)
+            new_year  = make_year_digit4(year)
+            new_month = make_digit2(month)
+            new_day   = make_digit2(day)
+
+            assert len(new_year)==4
+            assert len(new_month)==2
+            assert len(new_day)==2
+
+            if not valid_year(new_year): return None
+            if not valid_month(new_month): return None
+            if not valid_day(new_day): return None
+
+            basename_new = basename.replace(month,"MONTH").replace(day,"DAY").replace(year,"YEAR")
+            basename_new = basename_new.replace("MONTHDAYYEAR","MONTH-DAY-YEAR")
+            basename_new = basename_new.replace("MONTH.DAY.YEAR","MONTH-DAY-YEAR")
+            basename_new = basename_new.replace("MONTH",new_year).replace("DAY",new_month).replace("YEAR",new_day)
+            return os.path.join(dirname,basename_new)
+    return None
+        
+def interactive_fix_name(fname):
+    import sys
+    import os
+    if sys.platform=='darwin':
+        import readchar
+        getch = readchar.readchar
+    if sys.platform=='win32':
+        import msvcrt
+        getch = msvcrt.getch
+    fname_new = newname(fname)
+    if fname_new:
+        print("{} ==> {} ".format(fname,os.path.basename(fname_new)),end="")
+        print()
+        if os.path.exists(fname_new):
+            print("EXISTS")
+            return
+        print("[qnY] ",end="")
+        ch = getch().lower()
+        print(ch)
+        if ch in "q":
+            exit(0)
+        if ch in "y \n\r":
+            os.rename(fname,fname_new)
+            
+if __name__=="__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Seek out and rename MDY timestamps',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("roots",  nargs="+", help="Files/directories to search")
+
+    args = parser.parse_args()
+
+    for root in args.roots:
+        for (dirpath, dirnames, filenames) in os.walk(root):
+            if is_skipdir(dirpath):
+                continue        # don't do this directory
+            for filename in filenames:
+                fname = os.path.join(dirpath,filename)
+                interactive_fix_name(fname)
+    
---
+++ fix_timestamps_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python36
+#
+# fix_timestamps.py:
+# Scan the filesystem for timestamps in MMDDYY format and change them to YYYY-MM-DD
+#
+
+from fix_timestamps import *
+
+def test_newname():
+    pats = [["name.101289.jpg","name.1989-10-12.jpg"],
+            ["name.1.2.89.jpg","name.1989-01-02.jpg"],
+            ["photo-04022014.bmp","photo-2014-04-02.bmp"],
+            ["photo-04.02.2014.jpeg","photo-2014-04-02.jpeg"],
+            ]
+    for (old,new) in pats:
+        assert newname(old)==new
+
+def test_is_skipdir():
+    assert is_skipdir("/foo/assets/bar")==True
+    assert is_skipdir("/foo/bar")==False
+
+def test_is_skip_pat():
+    assert is_skip_pat("03.10.2016")==False
+    assert is_skip_pat("2016-03-10")==True
+    assert is_skip_pat("2016-30-01")==False
+    assert is_skip_pat("JetsamEvent-2017-03-31-063700.ips")==True
+
+def test_make_year_digit4():
+    assert make_year_digit4("89")=="1989"
+    assert make_year_digit4("14")=="2014"
+
+def test_make_digit2():
+    assert make_digit2("3")  == "03"
+    assert make_digit2("13") == "13"
+
+def test_valid_year():
+    print(valid_year(1985))
+    assert valid_year(1950) == False
+    assert valid_year(1985) == True
+    assert valid_year(2025) == True
+    assert valid_year(2085) == False
+
+def test_newname():
+    assert newname("SE/JetsamEvent-2017-03-31-063700.ips") == None
