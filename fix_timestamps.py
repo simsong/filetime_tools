7740e1418aebc74702e85d72f6040eb25409dfd8
Simson Garfinkel <simsong@acm.org>
2017-06-05 00:27:50 -0400

    added rename panel

--- fix_timestamps.py
+++ fix_timestamps.py
@@ -11,7 +11,7 @@ import sys
 import os
 import re
 
-debug = True
+debug = False
 
 # Patterns to skip
 skip_pats = [re.compile("[0-9a-f]{16,}",re.I), # 16 hex digits
@@ -34,6 +34,9 @@ skip_dirs = set(["/assets/","/mail downloads/", "conda-meta", "site-packages"])
 # Extensions to skip
 skip_exts = set([".css",".js", ".jar", ".webhistory", ".ics"])
 
+MONTHS = {"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12}
+
+my_re = re.compile("(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)([ .,_-]*)([12][89012][0-9][0-9])[^0-9]")
 
 def is_skipdir(fname):
     fname_lower = fname.lower()
@@ -114,6 +117,14 @@ def newname(fname):
                 basename_new = basename_new.replace("MONTH.DAY.YEAR","MONTH-DAY-YEAR")
                 basename_new = basename_new.replace("MONTH",new_year,1).replace("DAY",new_month,1).replace("YEAR",new_day,1)
             return os.path.join(dirname,basename_new)
+
+    # Look for a month and a year
+    m = my_re.search(basename)
+    if m:
+        month = MONTHS[m.group(1)]
+        year  = int(m.group(3))
+        basename_new = basename.replace(m.group(1)+m.group(2)+m.group(3),f"{year:04}-{month:02}")
+        return os.path.join(dirname,basename_new)
     return None
         
 def getch():
@@ -125,9 +136,15 @@ def getch():
         return msvcrt.getch().decode('utf-8')
     assert(0)
     
+def prompt(p):
+    if args.dry_run: return 'n'
+    print("[{}] ".format(p),end="")
+    ch = getch().lower()
+    print(ch)
+    return ch
 
 def file_renamer(fname,fname_new):
-    if debug:
+    if debug or args.dry_run:
         print("file_renamer({},{})".format(fname,fname_new))
         return
     if os.path.exists(fname) and not os.path.exists(fname_new):
@@ -142,25 +159,88 @@ def cui_fix_name(fname,fname_new):
         if os.path.exists(fname_new):
             print("EXISTS")
             return
-        print("[qnY] ",end="")
-        ch = getch().lower()
-        print(ch)
+        ch = prompt("qnY")
         if ch in "q":
             exit(0)
         if ch in "y \n\r":
             file_renamer(fname,fname_new)
             
+from PyQt5.QtWidgets import QWidget,QLabel
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5 import QtCore
+
+class DropWidget(QLabel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+    def dropEvent(self, event):
+        for url in event.mimeData().urls():
+            path = url.toLocalFile()
+            print(path)
+
+
+# http://zetcode.com/gui/pyqt5/dragdrop/
+# https://stackoverflow.com/questions/8568500/pyqt-getting-file-name-for-file-dropped-in-app
+# https://pythonspot.com/en/pyqt5-drag-and-drop/
+class DragWindow(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.label = DropWidget("Drag File to Change Name", self)
+        self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.label.setAlignment(Qt.AlignCenter)
+        #self.label.setStyleSheet("QLabel {background-color: red;}")
+        self.label.setStyleSheet("color: black; border: 1px solid red; background-color: white")
+        #self.label.dropped.connect(self.handleDropped)
+
+        self.button = QPushButton("Undo", self)
+        self.button.clicked.connect(self.undo)
+        self.listWidget = QListWidget()
+
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.label, 0, 0)
+        self.layout.addWidget(self.button, 0, 1)
+
+        self.layout.addWidget(QLabel("Add Prefix"), 1, 0)
+        self.prefix = QLineEdit()
+        self.layout.addWidget(self.prefix, 1, 1)
+
+        self.layout.addWidget(QLabel("Add Suffix"), 2, 0)
+        self.suffix = QLineEdit()
+        self.layout.addWidget(self.suffix, 2, 1)
+
+        self.layout.addWidget(self.listWidget, 3, 0, 1, 2, Qt.AlignCenter)
+
+        self.setLayout(self.layout)
+        self.show()
+
+    def undo(self):
+        print("UNDO")
+
 
 if __name__=="__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Seek out and rename MDY timestamps',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--drag", action="store_true")
     parser.add_argument("--gui", action='store_true')
-    parser.add_argument("--test", help="explain how a file will change")
+    parser.add_argument("--test", help="explain how a file would change")
+    parser.add_argument("--dry-run", action='store_true', help="just print, don't do it.")
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("roots",  nargs="*", help="Files/directories to search")
 
     args = parser.parse_args()
+
+    if args.debug:
+        debug = True
 
     movelist = []
 
@@ -168,18 +248,32 @@ if __name__=="__main__":
         print("{} => {}".format(args.test,newname(args.test)))
         exit(0)
 
+    def process(fname):
+        fname_new = newname(fname)
+        if fname_new:
+            if args.gui:
+                movelist.append([fname,fname_new])
+            else:
+                cui_fix_name(fname,fname_new)
+
     for root in args.roots:
+        if os.path.isfile(root):
+            process(root)
+            continue
+
         for (dirpath, dirnames, filenames) in os.walk(root):
             if is_skipdir(dirpath):
                 continue        # don't do this directory
             for filename in filenames:
-                fname = os.path.join(dirpath,filename)
-                fname_new = newname(fname)
-                if fname_new:
-                    if args.gui:
-                        movelist.append([fname,fname_new])
-                    else:
-                        cui_fix_name(fname,fname_new)
+                process(os.path.join(dirpath,filename))
+
+    if args.drag:
+        from PyQt5.QtWidgets import QApplication
+        app = QApplication(sys.argv)
+        win = DragWindow()
+        win.show()
+        sys.exit(app.exec_())
+
     if args.gui:
         from PyQt5.QtWidgets import QApplication
         from fileMoverDialog2 import VerifyDialog
